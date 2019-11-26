f524c60291d91e33587f2d5535805538b900d6d3
Owen <owen151128@gmail.com>
2019-11-26 11:30:13 +0900

    Project DefaultSet

---
+++ src/main/java/kr/owens/unittestexample/Application.java
@@ -0,0 +1,15 @@
+package kr.owens.unittestexample;
+
+/**
+ * @author owen151128@gmail.com
+ * <p>
+ * Created by owen151128 on 2019-11-26 11:22
+ * <p>
+ * Providing features related to Application class
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
